8edeaa1f00979814b05e29ae209bc02e303f9bd2
gaurav6386 <60260013+gaurav6386@users.noreply.github.com>
2020-09-23 08:55:35 +0530

    Final commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/models/message_model.dart';
+import 'package:flutter_chat_ui/models/user_model.dart';
+
+class ChatScreen extends StatefulWidget {
+  final User user;
+  ChatScreen({this.user});
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  _buildMessage(Message message, bool isMe) {
+    final Container msg = Container(
+      margin: isMe
+          ? EdgeInsets.only(
+        top: 8.0,
+        bottom: 8.0,
+        left: 80.0,
+      )
+          : EdgeInsets.only(
+        top: 8.0,
+        bottom: 8.0,
+//                  right: 80.0,     //Causes overflow error. So we need to remove it.
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * 0.75,
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe
+            ? BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          bottomLeft: Radius.circular(15.0),
+        )
+            : BorderRadius.only(
+          topRight: Radius.circular(15.0),
+          bottomRight: Radius.circular(15.0),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment:
+        isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 7.0),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if(isMe){
+     return msg;
+    }
+
+    return Row(
+      children: <Widget>[
+        msg,               // try
+        IconButton(
+          icon: message.isLiked ? Icon(Icons.favorite):Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked ? Theme.of(context).primaryColor : Colors.blueGrey,
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+  _buildMessageComposer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.photo),
+            iconSize: 30.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+          Expanded(
+            child: TextField(
+              textCapitalization: TextCapitalization.sentences,   //to capitalise the first letter of words of each sentences.
+              onChanged: (value) {},
+              decoration: InputDecoration.collapsed(  //'.collapsed' just removes the line under the textfield.
+                hintText: 'Send a Message...',
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            iconSize: 30.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          widget.user.name,
+          style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            iconSize: 25.0,
+            onPressed: () {},
+          ),
+        ],
+        elevation: 0.0,
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(), // To dropdown the keyboard on selecting on the chat screen without manually clicking on done or ddown button.
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    topLeft: Radius.circular(30.0),
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    topLeft: Radius.circular(30.0),
+                  ),
+                  child: ListView.builder(
+                    reverse:
+                        true, // What it does is reverse the whole list tile and starts from bottom.
+                    padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                    },
+                  ),
+                ),
+              ),
+            ),
+            _buildMessageComposer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
